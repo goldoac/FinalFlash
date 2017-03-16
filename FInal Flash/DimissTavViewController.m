40c39498d192cbe1295772b07f86c1fa5f7f4c03
ocastillo <ocastillo@applaudostudios.com>
2017-03-16 12:13:27 -0600

    Modal solution while  we get ride of 2 navigations

---
+++ FInal Flash/DimissTavViewController.m
@@ -0,0 +1,40 @@
+//
+//  DimissTavViewController.m
+//  FInal Flash
+//
+//  Created by applaudito on 3/16/17.
+//  Copyright © 2017 applaudito. All rights reserved.
+//
+
+#import "DimissTavViewController.h"
+
+@interface DimissTavViewController ()
+
+@end
+
+@implementation DimissTavViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)dimiss:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
